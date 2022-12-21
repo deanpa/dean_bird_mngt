b398da8bb28ed8cb883c636b167435663566ea00
Mandy Barron <barronm@landcareresearch.co.nz>
2022-12-21 16:08:22 +1300

    getting uptodate

--- simulationStart.py
+++ simulationStart.py
@@ -16,7 +16,8 @@ class CmdArgs(object):
 
 cmdargs = CmdArgs()
 
-params = simulationParams.PreyParams(cmdargs.species, int(cmdargs.scenario))
+#params = simulationParams.PreyParams(cmdargs.species, int(cmdargs.scenario))
+params = simulationParams.PreyParams("Kea", int("1"))
 
 simulationMain.main(params)
 
